57c67c91a39cf1d1ae9026f004f1cf92c4d11f03
yalab <rudeboyjet@gmail.com>
2013-08-03 16:26:31 +0900

    Implement one file sync with client side encrypt.

--- lib/s5/sync.rb
+++ lib/s5/sync.rb
@@ -1,5 +1,10 @@
 class S5::Sync
   attr_reader :bucket
+
+  def self.encrypt_key_path
+    ENV['HOME'] + '/.s5.key'
+  end
+
   def initialize(path, bucket: nil)
     @path = path
     @bucket = if bucket
@@ -13,9 +18,21 @@ class S5::Sync
       bucket = s3.buckets.create(@bucket)
     end
     @s3_object = bucket.objects[File.basename(@path)]
+    @options = {}
+  end
+
+  def encrypt!
+    key_path = self.class.encrypt_key_path
+    unless File.exists?(key_path)
+      File.open(key_path, 'w:BINARY', 0600) do |f|
+        f.write OpenSSL::Cipher::AES256.new(:CBC).random_key
+      end
+    end
+    @options[:server_side_encryption] = :aes256
+    @options[:encryption_key] = File.binread(key_path)
   end
 
   def run
-    @s3_object.write(File.read(@path))
+    @s3_object.write(File.binread(@path), @options)
   end
 end
--- test/s5/sync_test.rb
+++ test/s5/sync_test.rb
@@ -2,13 +2,25 @@ require 'test_helper'
 
 class S5::SyncTest < MiniTest::Unit::TestCase
   def setup
-    @plain = Digest::SHA2.hexdigest(Time.now.to_f.to_s)
+    @encrypt_key_path = S5::Sync.encrypt_key_path
+    @encrypt_key_path_backup = @encrypt_key_path + '.s5_test'
+    if File.exists?(@encrypt_key_path)
+      FileUtils.mv @encrypt_key_path, @encrypt_key_path_backup
+    end
+
+    @plain = Digest::SHA2.hexdigest(Time.now.to_f.to_s) + 'test'
     @path = File.expand_path('../../fixtures/test.txt', __FILE__)
     FileUtils.mkdir_p File.dirname(@path)
     File.open(@path, 'w') do |f|
-      f.puts @plain
+      f.write @plain
     end
     @sync = S5::Sync.new(@path)
+  end
+
+  def teardown
+    if File.exists?(@encrypt_key_path_backup)
+      FileUtils.mv @encrypt_key_path_backup, @encrypt_key_path
+    end
   end
 
   def test_default_bucket_name
@@ -17,6 +29,18 @@ class S5::SyncTest < MiniTest::Unit::TestCase
 
   def test_sync_run
     s3_object = @sync.run
-    assert_equal @plain, s3_object.read.chop
+    assert_equal @plain, s3_object.read
+  end
+
+  def test_encrypt_key_path
+    assert_equal ENV['HOME'] + '/.s5.key', S5::Sync.encrypt_key_path
+  end
+
+  def test_sync_run_with_encrypt
+    @sync.encrypt!
+    assert File.exists?(S5::Sync.encrypt_key_path)
+    s3_object = @sync.run
+    refute_equal @plain, s3_object.read
+    assert_equal @plain, s3_object.read(encryption_key: File.binread(@encrypt_key_path))
   end
 end
