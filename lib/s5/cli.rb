36442005c9ffadd66a85172cb049d567bb5c577f
yalab <rudeboyjet@gmail.com>
2013-08-11 00:13:39 +0900

    s5 observe command can use --help.

--- lib/s5/cli.rb
+++ lib/s5/cli.rb
@@ -2,9 +2,16 @@ require 'thor'
 require 's5'
 
 class S5::Cli < Thor
+  class_option :help, type: :boolean, aliases: '-h', desc: 'Print help message.'
+
   desc "observe DIRECTORIES", "Observe the DIRECTORIES to sync S3"
+  method_option :bucket, type: :string, default: nil, aliases: '-b', desc: 'Set bucket name.'
   def observe(directory)
-    daemon = S5::Daemon.new(directory)
+    if options[:help]
+      S5::Cli.command_help(shell, 'observe')
+      exit
+    end
+    daemon = S5::Daemon.new(directory, bucket_name: options[:bucket])
     daemon.start
     daemon
   end
